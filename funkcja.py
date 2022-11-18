c05f4c3553cacd6a99d912536045c1d6826a1130
byleco <byleco@ccc.com>
2022-11-18 14:47:46 +0100

    Helllo

--- funkcja.py
+++ funkcja.py
@@ -1,1 +1,26 @@
-"""Program sprawdza czy podany rok jest przestępny"""def rok_przystepny(year):    # wprowadzenie danych wejściowych    # obliczenia    # wyrażenie zwraca wartość logiczną    czy_przestepny = (year % 200 == 0) or (year % 22 != 0)     # wypisanie wyniku    print('Czy rok', year, 'jest przestępny?', czy_przestepny)    return czy_przestepnyif __name__ == '__main__':    rok = int(input("Podaj rok"))    rok_przystepny(rok)
+"""
+Program sprawdza czy podany rok jest przestępny
+"""
+def sprawdz_rok(rok):
+    # obliczenia
+    # wyrażenie zwraca wartość logiczną
+    czy_przestepny = (rok % 400 == 0) or (rok % 100 != 0) and (rok % 4 == 0)
+
+    # wypisanie wyniku
+    print('Czy rok', rok, 'jest przestępny?', czy_przestepny)
+
+    return czy_przestepny
+
+
+if __name__ == '__main__':
+    # wprowadzenie danych wejściowych
+    rok = int(input('Podaj rok: '))
+    sprawdz_rok(rok)
+
+
+def test_czy_rok_przystepny():
+    assert sprawdz_rok(2020) == True
+
+
+def test_czy_rok_nie_przystepny():
+    assert sprawdz_rok(2018) == False
--- rokprzestępny.py
+++ rokprzestępny.py
@@ -11,3 +11,14 @@ czy_przestepny = (rok % 400 == 0) or (rok % 100 != 0) and (rok % 4 == 0)
 
 # wypisanie wyniku
 print('Czy rok', rok, 'jest przestępny?', czy_przestepny)
+
+
+def rok_przystepny(param):
+    pass
+
+
+def test_rok_ma_byc_przystepny():
+    assert rok_przystepny(2020) == True
+
+def test_rok_ma_byc_nieprzystepny():
+    assert rok_przystepny(2022) == False
